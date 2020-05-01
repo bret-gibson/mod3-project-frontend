0593a8b779e34da0a73f91a62d7085c8fe592d85
bret-gibson <bret.gibson@gmail.com>
2020-05-01 16:03:10 -0400

    Render audio and fixed buttons

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const audio = document.getElementById("current-game-song")
-audio.volume = 0.0
-
-
 document.addEventListener("DOMContentLoaded", () => {
-    // renderAudio()
-    audio.addEventListener("play", fetchChoices)
+    fetchGameSong()
+    // const audio = document.getElementById("current-game-song")
+    
 })
 
+function fetchGameSong(){
+    fetch("http://localhost:3000/game_songs")
+    .then(response => response.json())
+    .then(gameSongData => {
+        const gameSongArray = gameSongData["data"]
+        const songChoice = gameSongArray[Math.floor(Math.random() * gameSongArray.length)]
+        const songUrl = songChoice.attributes.song.source
+        renderAudio(songChoice)
+    })
+}
 
-// function renderAudio (){
-//     const audio = document.getElementById("current-game-song")
-//     audio.volume = 0.5
-// }
+function renderAudio(songChoice){
+    const audioPlayer = document.createElement("audio")
+    const audioContainer = document.getElementById("audio-container")
+    audioPlayer.id = "current-game-song"
+    audioPlayer.controls = "controls"
+    const songUrl = songChoice.attributes.song.source 
+    audioPlayer.src = songUrl   //gamesong
+    audioPlayer.type="audio/mp3"
+    audioPlayer.volume = 0.0
+    audioPlayer.addEventListener("play", () => fetchChoices(songChoice), { once: true})
+    audioContainer.append(audioPlayer)
 
-function fetchChoices(){
+}
+
+function fetchChoices(songChoice){
     //get fetch call to game songs, where gamesong = false
     //create buttons for each
     //do this X amount of times for X choices
@@ -22,30 +38,31 @@ function fetchChoices(){
     .then(response => response.json())
     .then(songs => {
         const songArray = songs["data"]
-        const dummySongs = songArray.filter(song => song.attributes.dummy === true) //array of only dummy songs
+        const dummySongs = songArray.filter(song => song.attributes.dummy === true).sort(() => Math.random() - 0.5); //array of only dummy songs
+
         let choices = []
-        let pick = dummySongs[Math.floor((Math.random() * dummySongs.length))]
-        choices.push(pick)
-        while (choices.length <= 3){
-           let newPick = dummySongs[Math.floor((Math.random() * dummySongs.length))]
-            choices.forEach(element => {
-                if (element !== newPick){
-                    choices.push(newPick)
-                }
-            })
+
+        for (let i = 0; choices.length < 3; i++){
+            choices.push(dummySongs[i].attributes)
         }
-        choices.pop()
-        choiceButtons(choices)
-    })
+        choices.push(songChoice.attributes.song)
+        renderChoices(choices)
+    }) 
+}
 
- }
-
-function choiceButtons(choices){
+function renderChoices(choices){
+    //buttons for the wrong answers
+    // shuffle buttons to make order random
+    choices.sort(() => Math.random() - 0.5);
+    const choiceContainer = document.querySelector("#choice-container")
     choices.forEach((choice) => {
         let button = document.createElement("button")
-        button.innerText = `${choice.attributes.title} - ${choice.attributes.artist}`
+        button.innerText = `${choice.title} - ${choice.artist}`
         
-        const choiceContainer = document.querySelector("#choice-container")
         choiceContainer.append(button)
     })
+    const mainContainer = document.querySelector("#main-container")
+    mainContainer.append(choiceContainer)
+
+    //button for the right answer
 }
