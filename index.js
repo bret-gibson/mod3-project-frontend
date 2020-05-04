35bcbebf50ecf86d56e54b6f25439d3952bcf9b3
bret-gibson <bret.gibson@gmail.com>
2020-05-04 04:54:41 -0400

    added t10 second timer and basic points functionality

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+let countdown
+let timerId
+let score = 0
+const mainContainer = document.querySelector("#main-container")
+const audioContainer = document.querySelector("#audio-container")
+const choiceContainer = document.querySelector("#choice-container")
+const gameHeader = document.querySelector("#game-header")
+const choiceList = document.querySelector("#choices")
+
 document.addEventListener("DOMContentLoaded", () => {
-    fetchGameSong()    
+    fetchGameSong()
 })
 
 function fetchGameSong(){
-    const audioContainer = document.getElementById("audio-container")
     audioContainer.innerHTML = ""
-    const choiceContainer = document.querySelector("#choice-container")
-    choiceContainer.innerHTML = ""
+    choiceList.innerHTML = ""
     fetch("http://localhost:3000/game_songs")
     .then(response => response.json())
     .then(gameSongData => {
@@ -18,14 +25,15 @@ function fetchGameSong(){
 }
 
 function renderAudio(songChoice){
+    const timer = document.querySelector("#timer")
+    timer.textContent = "10 seconds remaining"
     const audioPlayer = document.createElement("audio")
-    const audioContainer = document.getElementById("audio-container")
     audioPlayer.id = "current-game-song"
     audioPlayer.controls = "controls"
     const songUrl = songChoice.attributes.song.source 
     audioPlayer.src = songUrl   //gamesong
     audioPlayer.type="audio/mp3"
-    audioPlayer.volume = 0.0
+    audioPlayer.volume = 0.1
     audioPlayer.addEventListener("play", () => fetchChoices(songChoice), { once: true})
     audioContainer.append(audioPlayer)
 }
@@ -34,11 +42,11 @@ function fetchChoices(songChoice){
     //get fetch call to game songs, where gamesong = false
     //create buttons for each
     //do this X amount of times for X choices
+    startTimer(9)
 
     fetch("http://localhost:3000/songs")
     .then(response => response.json())
     .then(songs => {
-
         // Maybe fix so that we arent querying ALL songs every time...
         // want to query for JUST the songs marked as dummy (may need to change API)
         const songArray = songs["data"]
@@ -57,9 +65,10 @@ function fetchChoices(songChoice){
 function renderChoices(choices, songChoice){
     // buttons for the wrong answers
     // shuffle buttons to make order random
-    let choices1 = choices.sort(() => Math.random() - 0.5);
-    const choiceContainer = document.querySelector("#choice-container")
-    choices1.forEach((choice) => {
+    let new_choices = choices.sort(() => Math.random() - 0.5);
+    new_choices.forEach((choice) => {
+        let choiceItem = document.createElement("li")
+        choiceItem.textContent =""
         let button = document.createElement("button")
         
         if (choice["attributes"]){
@@ -71,9 +80,9 @@ function renderChoices(choices, songChoice){
 
         button.id = choice.id
         button.addEventListener("click", () => handleChoice(choices, songChoice))
-        choiceContainer.append(button)
+        choiceItem.append(button)
+        choiceList.append(choiceItem)
     })
-    const mainContainer = document.querySelector("#main-container")
     mainContainer.append(choiceContainer)
 }
 
@@ -82,17 +91,36 @@ function handleChoice(choices, songChoice) {
     // debugger
     if (event.target.id === songChoice.id){
         alert("YOU GOT IT RIGHT")
+        clearInterval(timerId)
+        let points = document.getElementById('points')
+        score += 100
+        points.innerText = `${score} Points`
         fetchGameSong()
     } else {
         alert("WRONG! GAME OVER!")
+        clearInterval(timerId)
         //remove event listener so you can't keep playing
         //or remove buttons.. 
     }
-
-
     //if choice = gameSong, then give point
     //if not, then no point
+}
 
+function startTimer(duration){
+    let timeLeft = duration
+    let timer = document.getElementById('timer')
     
-
+    timerId = setInterval(countdown, 1000)
+    
+    function countdown() {
+      if (timeLeft < 0) {
+        clearTimeout(timerId)
+        const audio = document.querySelector("audio")
+        audio.pause()
+        alert("Out of time.. you lose.")
+      } else {
+        timer.textContent = timeLeft + ' seconds remaining'
+        timeLeft--
+      }
+    }
 }
